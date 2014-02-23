62f8910a4cb8212b3fffd009147f1c7d8f8c3928
Anders Goransson <anders.goransson@jayway.com>
2014-02-23 14:36:33 +0100

    Chapter 11 updates

--- EatApplication/Eat/src/main/java/com/eat/chapter11/Chapter11Activity.java
+++ EatApplication/Eat/src/main/java/com/eat/chapter11/Chapter11Activity.java
@@ -1,9 +1,12 @@
 package com.eat.chapter11;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.eat.R;
+import com.eat.chapter10.FileDownloadActivity;
 
 public class Chapter11Activity extends Activity {
 
@@ -11,5 +14,9 @@ public class Chapter11Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter11);
+    }
+
+    public void onBluetoothActivity(View v) {
+        startActivity(new Intent(this, BluetoothActivity.class));
     }
 }
