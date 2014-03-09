235675fa9229e92b7ad385f83537c8cfe7372b80
Anders Goransson <anders.goransson@jayway.com>
2014-03-09 15:57:43 +0100

    Chapter 8: Aligned SharedPreference example with template

--- EatApplication/Eat/src/main/java/com/eat/chapter8/SharedPreferencesActivity.java
+++ EatApplication/Eat/src/main/java/com/eat/chapter8/SharedPreferencesActivity.java
@@ -9,75 +9,109 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
 import android.view.View;
+import android.widget.TextView;
 
 import com.eat.R;
 
 
 public class SharedPreferencesActivity extends Activity {
 
+    TextView mTextValue;
+
+    /**
+     * Show read value in a TextView.
+     */
     private Handler mUiHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             if (msg.what == 0) {
                 Integer i = (Integer)msg.obj;
-                // Use the result
+                mTextValue.setText(Integer.toString(i));
             }
         }
     };
 
-    private class SharedPreferenceHandler extends Handler {
+    private class SharedPreferenceThread extends HandlerThread {
+
         private static final String KEY = "key";
         private SharedPreferences mPrefs;
-        static final int READ = 1;
-        static final int WRITE = 2;
+        private static final int READ = 1;
+        private static final int WRITE = 2;
 
-        private SharedPreferenceHandler(Looper looper, SharedPreferences prefs) {
-            super(looper);
-            this.mPrefs = prefs;
+        private Handler mHandler;
+
+        public SharedPreferenceThread() {
+            super("SharedPreferenceThread", Process.THREAD_PRIORITY_BACKGROUND);
+            mPrefs = getSharedPreferences("LocalPrefs", MODE_PRIVATE);
         }
 
         @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch(msg.what) {
-                case READ:
-                    mUiHandler.sendMessage(mUiHandler.obtainMessage(0, mPrefs.getInt(KEY, 0)));
-                    break;
-                case WRITE:
-                    SharedPreferences.Editor editor = mPrefs.edit();
-                    editor.putInt(KEY, msg.arg1);
-                    editor.commit();
-                    break;
-            }
+        protected void onLooperPrepared() {
+            super.onLooperPrepared();
+            mHandler = new Handler(super.getLooper()) {
+                @Override
+                public void handleMessage(Message msg) {
+                    switch(msg.what) {
+                        case READ:
+                            mUiHandler.sendMessage(mUiHandler.obtainMessage(0, mPrefs.getInt(KEY, 0)));
+                            break;
+                        case WRITE:
+                            SharedPreferences.Editor editor = mPrefs.edit();
+                            editor.putInt(KEY, (Integer)msg.obj);
+                            editor.commit();
+                            break;
+                    }
+                }
+            };
+        }
+
+        @Override
+        public Looper getLooper() {
+            throw new RuntimeException("Looper not public");
+        }
+
+        public void read() {
+            mHandler.sendEmptyMessage(READ);
+        }
+        public void write(int i) {
+            mHandler.sendMessage(Message.obtain(Message.obtain(mHandler, WRITE, i)));
         }
     }
 
-    private int i;
-    private HandlerThread mHandlerThread;
-    private SharedPreferenceHandler mSharedPreferencesHandler;
+    private int mCount;
+    private SharedPreferenceThread mThread;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared_preferences);
-        mHandlerThread = new HandlerThread("BgThread", Process.THREAD_PRIORITY_BACKGROUND);
-        mHandlerThread.start();
-        mSharedPreferencesHandler = new SharedPreferenceHandler(mHandlerThread.getLooper(), getSharedPreferences("LocalPrefs", MODE_PRIVATE));
+        mTextValue = (TextView) findViewById(R.id.text_value);
+        mThread = new SharedPreferenceThread();
+        mThread.start();
     }
 
+    /**
+     * Write dummy value from the UI thread.
+     */
     public void onButtonClickWrite(View v) {
-        mSharedPreferencesHandler.sendMessage(mSharedPreferencesHandler.obtainMessage(SharedPreferenceHandler.WRITE, i++));
+        mThread.write(mCount++);
     }
 
+    /**
+     * Initiate a read from the UI thread.
+     */
     public void onButtonClickRead(View v) {
-        mSharedPreferencesHandler.sendMessage(mSharedPreferencesHandler.obtainMessage(SharedPreferenceHandler.READ));
+        mThread.read();
     }
 
+    /**
+     * Ensure that the background thread is terminated with the Activity.
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mHandlerThread.quit();
+        mThread.quit();
     }
 }
